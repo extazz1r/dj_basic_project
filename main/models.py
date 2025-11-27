b1016049178dc21ddbaf579ee3c8f766ea4da9ec
extazz1r <extazz1r@Noutbuk-extazz1r.local>
2025-11-28 01:32:34 +0300

    added model Categoty, added urls.py in main app, added main application in settings dj_basic_project

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+ 
